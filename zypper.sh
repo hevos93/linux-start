2f2fb01eff8e8389ea01583ab923ea26f28137a9
hevos <hevos93@gmail.com>
2022-05-06 13:58:59 +0200

    Added base for DRY principle

--- zypper.sh
+++ zypper.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 sudo zypper in -y zsh git neofetch
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-cp configs/zshConfig $HOME/.zshrc
-cp configs/neofetchConfig $HOME/.config/neofetch/config.conf
+ ./base.sh
